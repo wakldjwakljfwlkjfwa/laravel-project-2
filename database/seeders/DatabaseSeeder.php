72b82f238201b38ff595cf47accfc7514cb216d7
tw <tw@tw>
2023-06-28 12:05:44 +0600

    Add users to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'email' => 'test1@example.com',
+        ]);
+        User::factory()->create([
+            'email' => 'test2@example.com',
+        ]);
+        User::factory()->create([
+            'email' => 'test3@example.com',
+        ]);
     }
 }
